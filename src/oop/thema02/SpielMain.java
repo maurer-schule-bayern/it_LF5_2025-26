7208c506178fcc7b4b65edaf7c2701c6b7c803a0
Florian Maurer <florian.maurer@schule.bayern.de>
2026-04-14 10:25:51 +0200

    added oop thema 02

---
+++ src/oop/thema02/SpielMain.java
@@ -0,0 +1,20 @@
+package oop.thema02;
+
+public class SpielMain {
+
+	public static void main(String[] args) {		
+		// Spiel initialisieren
+		TicTacToe game = new TicTacToe();
+
+		// Spielinformationen ausgeben
+		game.gameInfo();
+		
+		// Spiel ausführen
+		while (true) {
+			if (game.step()) {
+				break;
+			}
+		}
+	}
+
+}
