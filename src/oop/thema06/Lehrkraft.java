34aa892ac06d5a37ae45c0d9883db81c5d2f4348
Florian Maurer <florian.maurer@schule.bayern.de>
2026-05-12 10:08:04 +0200

    fix oop thema06

--- src/oop/thema06/Lehrkraft.java
+++ src/oop/thema06/Lehrkraft.java
@@ -8,7 +8,6 @@ public class Lehrkraft extends Person {
 		// TODO 
 	}
 
-	@Override
 	public void printInformation() {
 		System.out.println("Name: " + vorname + " " + nachname);
 		System.out.println("Alter: " + alter);
