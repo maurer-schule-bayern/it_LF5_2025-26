d1efa08f7a26bc2491aa772cfdfedaaddf7c8288
Florian Maurer <florian.maurer@schule.bayern.de>
2026-05-12 10:14:10 +0200

    fix oop Thema 06

--- src/oop/thema06/Person.java
+++ src/oop/thema06/Person.java
@@ -5,11 +5,7 @@ abstract public class Person {
 	protected String vorname;
 	protected String nachname;
 	
-	public Person(int alter, String vorname, String nachname) {
-		this.alter = alter;
-		this.vorname = vorname;
-		this.nachname = nachname;
-	}
+
 		
 	abstract public void printInformation();
 	
